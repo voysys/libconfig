2ec3be794b3ecc5d0509d387ad9ef876d6dce3b6
Torkel <torkel@voysys.se>
2020-08-26 21:47:15 +0900

    fix

--- lib/libconfig.c
+++ lib/libconfig.c
@@ -651,8 +651,8 @@ int config_read_file(config_t *config, const char *filename)
   || defined(WIN64) || defined(_WIN64) || defined(__WIN64__))
   const int filename_wsize = MultiByteToWideChar(CP_UTF8, 0, filename, -1, NULL, 0);
   const int mode_wsize = MultiByteToWideChar(CP_UTF8, 0, "rt", -1, NULL, 0);
-  wchar_t* filename_w = (wchar_t*)malloc(filename_wsize);
-  wchar_t* mode_w = (wchar_t*)malloc(mode_wsize);
+  wchar_t* filename_w = (wchar_t*)malloc(filename_wsize * sizeof(wchar_t));
+  wchar_t* mode_w = (wchar_t*)malloc(mode_wsize * sizeof(wchar_t));
   MultiByteToWideChar(CP_UTF8, 0, filename, -1, filename_w, filename_wsize);
   MultiByteToWideChar(CP_UTF8, 0, "rt", -1, mode_w, mode_wsize);
   FILE *stream = _wfopen((const wchar_t*)filename_w, (const wchar_t*)mode_w);
